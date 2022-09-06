10d4fd20c6fd290190ccfe97d01f8f48e93b9ec5
TrXGustavoo <gustavofp111@gmail.com>
2022-09-06 15:26:49 -0300

    Adição da opção de cadastro

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import HomeView
+from .views import HomeView, register
 
 
 
 urlpatterns = [
-    path('', HomeView.as_view(), name='home')
+    path('', HomeView.as_view(), name='home'),
+    path('accounts/register', register, name='register'),
 ]
